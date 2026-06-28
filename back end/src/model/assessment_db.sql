00012011b55fb69256d150e723a619e8c9b64a3a
Sanket Chavan <sanketc2102@gmail.com>
2026-06-28 12:25:34 +0530

    add database file

---
+++ back end/src/model/assessment_db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE customer_support_tickets_db;
+
+USE customer_support_tickets_db ;
+
+CREATE TABLE tickets (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    customerName VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    category ENUM('payment', 'technical', 'general') DEFAULT 'general',
+    status ENUM('open', 'in_progress', 'resolved') DEFAULT 'resolved',
+    description TEXT not null,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+SELECT * FROM tickets;
