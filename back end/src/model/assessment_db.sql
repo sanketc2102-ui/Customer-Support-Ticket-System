027084247dce27484c0d62816a2cb8c2d65fd073
Sanket Chavan <sanketc2102@gmail.com>
2026-06-28 12:28:03 +0530

    database file chages

--- back end/src/model/assessment_db.sql
+++ back end/src/model/assessment_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE tickets (
     customerName VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
     category ENUM('payment', 'technical', 'general') DEFAULT 'general',
-    status ENUM('open', 'in_progress', 'resolved') DEFAULT 'resolved',
+    status ENUM('open', 'in_progress', 'resolved') DEFAULT 'open',
     description TEXT not null,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
